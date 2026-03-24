90864272a8913e4f86a32e5fef742a6627379a51
Joao Liberato <joao.liberato@tecnico.ulisboa.pt>
2026-03-24 11:46:29 +0000

    refactors NavigationCursor to separate tree functionality

--- model/include/model.hpp
+++ model/include/model.hpp
@@ -19,24 +19,34 @@ struct MenuElement {
 
 using PathToElement = std::vector<int>;
 
+class XMBTree {
+	public:
+		std::shared_ptr<MenuElement> rootElement;
+		XMBTree(std::shared_ptr<MenuElement> rootElement);
+
+		/// @brief Places the given element at the specified path. If there is already an element at that path, it will be replaced and returned.
+		/// If the path is invalid (e.g. trying to place an element in a non-existent parent), an error status will be returned.
+		absl::StatusOr<std::shared_ptr<MenuElement>> placeElementInPath(PathToElement path, std::shared_ptr<MenuElement> elementToInsert);
+		/// @brief Retrieves the element at the specified path. If the path is invalid, an error status will be returned.
+		absl::StatusOr<std::shared_ptr<MenuElement>> getElementAtPath(PathToElement path) const;
+		/// @brief Retrieves the parent of the element at the specified path. If the path is invalid or if the element at
+		///  that path is the root (which has no parent), an error status will be returned.
+		absl::StatusOr<std::shared_ptr<MenuElement>> getChildrenOfElementAtPath(PathToElement path) const;
+};
+
 class NavigationCursor {
-	std::shared_ptr<MenuElement> rootElement;
+	std::shared_ptr<XMBTree> tree;
 	PathToElement elementPath;
 	public:
-		NavigationCursor(std::shared_ptr<MenuElement> element);
-		absl::StatusOr<std::shared_ptr<MenuElement>> getCurrentElement() const;
-		absl::StatusOr<std::shared_ptr<MenuElement>> getParentOfCurrentElement() const;
-		absl::StatusOr<std::shared_ptr<MenuElement>> getElementAtPath(PathToElement path) const;
-		absl::StatusOr<std::shared_ptr<MenuElement>> getCurrentElementPath() const;
-		absl::StatusOr<std::vector<std::shared_ptr<MenuElement>>> getCurrentElementChildren() const;
-		int getCurrentMenuIndex() const;
+		NavigationCursor(std::shared_ptr<XMBTree> tree);
 
-		absl::StatusOr<std::shared_ptr<MenuElement>> insertElementIntoElementPath(PathToElement path, std::shared_ptr<MenuElement> element, int insertIndex);
-		absl::StatusOr<std::shared_ptr<MenuElement>> insertElementIntoCurrentElement(std::shared_ptr<MenuElement> element, int insertIndex);
+		absl::StatusOr<std::shared_ptr<MenuElement>> getCurrentElement() const;
+
+		int getCurrentMenuIndex() const;
 
 		absl::Status moveNext();
 		absl::Status movePrevious();
-		absl::Status moveInto(int moveIntoIndex);
+		absl::Status moveInto(int innerIndex);
 		absl::Status moveOut();
 };
 
--- model/src/model.cpp
+++ model/src/model.cpp
@@ -3,46 +3,13 @@
 #include <memory>
 
 
-NavigationCursor::NavigationCursor(std::shared_ptr<MenuElement> rootElement) : rootElement(rootElement) {}
+NavigationCursor::NavigationCursor(std::shared_ptr<XMBTree> tree) : tree(tree) {}
 
 absl::StatusOr<std::shared_ptr<MenuElement>> NavigationCursor::getCurrentElement() const {
-	std::shared_ptr<MenuElement> currentElement = rootElement;
-	for (int index : elementPath) {
-		if (!currentElement->children.has_value() || index >= currentElement->children->size()) {
-			return absl::NotFoundError("Invalid path: index path out of bounds at index " + std::to_string(index));
-		}
-		currentElement = (*currentElement->children)[index];
-	}
-	return currentElement;
+	return tree->getElementAtPath(elementPath);
 }
 
 int NavigationCursor::getCurrentMenuIndex() const {
 	CHECK_GT(elementPath.size(), 0) << "Element path size needs to be greater than 0.";
 	return elementPath.back();
-}
-
-absl::StatusOr<std::shared_ptr<MenuElement>> NavigationCursor::getParentOfCurrentElement() const {
-	if (elementPath.empty()) {
-		return absl::NotFoundError("Current element is the root element, which has no parent.");
-	}
-	std::shared_ptr<MenuElement> currentElement = rootElement;
-	for (size_t i = 0; i < elementPath.size() - 1; ++i) {
-		int index = elementPath[i];
-		if (!currentElement->children.has_value() || index >= currentElement->children->size()) {
-			return absl::NotFoundError("Invalid path: index path out of bounds at index " + std::to_string(index));
-		}
-		currentElement = (*currentElement->children)[index];
-	}
-	return currentElement;
-}
-
-absl::Status NavigationCursor::moveNext() {
-	auto currentElementOrStatus = getCurrentElement();
-	if (!currentElementOrStatus.ok())
-		return currentElementOrStatus.status();
-	std::shared_ptr<MenuElement> currentElement = currentElementOrStatus.value();
-	auto parentElement = getParentOfCurrentElement();
-	if(!parentElement.ok())
-		return parentElement.status();
-	std::shared_ptr<MenuElement> parentElementValue = parentElement.value();	
 }
